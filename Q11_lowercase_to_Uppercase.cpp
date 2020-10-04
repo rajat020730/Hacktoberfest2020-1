fa8ae362c3fdb00aa5141505aa8da737145a5e21
Rajat Agarwal <rajat020730@gmail.com>
2020-10-04 12:32:56 +0530

    Update Q11_lowercase_to_Uppercase.cpp

--- Q11_lowercase_to_Uppercase.cpp
+++ Q11_lowercase_to_Uppercase.cpp
@@ -1,12 +1,19 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 using namespace std;
-int main() {
-    int i,n;
-    string str;
-    cin>>str; //taking input of string
-    n=str.length(); //computing length of string
-    for(i=0;i<n;i++){
-        str[i]^=32; //changing case of each letter separately using bit manipulation.
-    }
-    cout<<str; //printing the modified string
+int main()
+{
+   string s;
+   int i;
+   cout<<"Enter the String: "; 
+   getline(cin,s);
+   int n=s.length();
+   for(i=0;i<=n;i++) { 
+      if(s[i]>=97 && s[i]<=122)
+      {
+		s[i]=s[i]-32;
+      }
+   }
+   cout<<"Modified string: "<<s;
+   return 0;
 }
